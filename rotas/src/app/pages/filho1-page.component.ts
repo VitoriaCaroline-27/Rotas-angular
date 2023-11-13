fff5712162fe5c69cfb06497a3a92ee0cf505b6e
landing-page2 <viihcarol34@gmail.com>
2023-11-13 09:35:35 -0300

    first commit

---
+++ rotas/src/app/pages/filho1-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'app-filho-1',
+  templateUrl: './filho1-page.component.html'
+})
+
+export class Filho1PageComponent{
+  
+
+}
